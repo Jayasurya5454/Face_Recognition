31b4f57003e03e5352c9b293c26bf82ab81d0a7c
Jayasurya M <123261802+Jayasurya5454@users.noreply.github.com>
2023-12-11 18:47:07 +0530

    Update Human_FaceCount.py

--- Human_FaceCount.py
+++ Human_FaceCount.py
@@ -2,40 +2,26 @@ import cv2
 from matplotlib import pyplot as plt
 import msvcrt
 
-# Load the pre-trained face detection classifier
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-# Open a video capture object (0 corresponds to the default camera)
 cap = cv2.VideoCapture(0)
 
 while True:
-    # Read a frame from the camera
     ret, frame = cap.read()
-
-    # Convert the frame to grayscale for face detection
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Detect faces in the frame
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
-
-    # Draw rectangles around the detected faces
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-    # Display the count of detected faces in real-time
     face_count = len(faces)
     cv2.putText(frame, f'Faces: {face_count}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-    # Display the resulting frame using matplotlib
     plt.imshow(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
     plt.title('Face Detection')
     plt.draw()
     plt.pause(0.01)
-
-    # Check for keyboard input to exit the loop
     if msvcrt.kbhit() and msvcrt.getch() == b'q':
         break
 
-# Release the video capture object and close all windows
 cap.release()
 cv2.destroyAllWindows()
